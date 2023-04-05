ac28f88c6febeb0a175f648e410ba4abdce394ec
Caio <129826533+dib10@users.noreply.github.com>
2023-04-04 21:18:53 -0300

    Update jogobicho2.py

--- jogobicho2.py
+++ jogobicho2.py
@@ -52,7 +52,6 @@ def obter_animal(numero):
     else:
         return "Vaca - Grupo 25 🐄"
 
-
 print("\033[32mBem Vindo ao simulador de jogo do bicho 🐊\033[0m")
 
 aposta = int(input("Aposte um número (0-9999): "))
@@ -62,7 +61,6 @@ sorteio = random.randint(0, 9999)
 aposta_ultimos_dois = aposta % 100
 sorteio_ultimos_dois = sorteio % 100
 
-
 animal_aposta = obter_animal(aposta_ultimos_dois)
 animal_sorteado = obter_animal(sorteio_ultimos_dois)
 
@@ -70,7 +68,6 @@ print("\033[1;36mNúmero sorteado:\033[m", sorteio)
 print("\033[1;36mAnimal sorteado:\033[m", animal_sorteado)
 print("\033[33mSeu número apostado:\033[m", aposta)
 print("\033[33mSeu animal apostado:\033[m", animal_aposta)
-
 
 if aposta == sorteio:
     print("\033[0;32mParabéns, você ganhou a aposta!\033[m")

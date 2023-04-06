2aa77439e5bd8274d30885286668e9a6c478ac65
Caio <129826533+dib10@users.noreply.github.com>
2023-04-06 19:50:52 -0300

    Update jogobicho2.py
    Começo da implementação das modalidades do jogo do bicho: 
    
    Observação: Nesse código postado só foi trabalhado a modalidade dezena, as outras virão após uma atualização do código.

--- jogobicho2.py
+++ jogobicho2.py
@@ -47,49 +47,43 @@ def obter_animal(numero):
         return "Tigre - Grupo 22 🐅"
     elif numero in range(89, 93):
         return "Urso - Grupo 23 🐻"
-    elif numero in range(93, 97):
+    elif numero in range(93, 97): #range no final sempre pega um número a menos
         return "Veado - Grupo 24 🦌"
     else:
         return "Vaca - Grupo 25 🐄"
 
 print("\033[32mBem Vindo ao simulador de jogo do bicho 🐊\033[0m")
 
-aposta = None
-while True:
-    try:
-        aposta = int(input("Aposte um número (0-9999): "))
-        if aposta < 0 or aposta > 9999:
-            print("\033[31mNúmero inválido, tente novamente.\033[0m")
-        else:
-            break
-    except ValueError:
-        print("\033[31mEntrada inválida, tente novamente.\033[0m")
+print("🦁Escolha uma modalidade para jogar")
+modalidades = {
+    1: "Dezena",
+    2: "Centena",
+    3: "Milhar",
+    4: "Grupo"
+}
+for k, v in modalidades.items():
+    print(f"[{k}] {v}") ## k seria key e v seria value, para representar cada item e seu correspondente
+modalidade = 0
+while not 1 <= modalidade <= 4: #vericar se o usuário digita uma das modalidades
+    modalidade = int(input("Digite o número da modalidade: "))
+    if not 1 <= modalidade <= 4:
+        print("\033[31mModalidade inválida! Digite um número de 1 a 4.\033[0m")
 
+print("🦁Modalidade selecionada:", modalidades[modalidade])
 
-sorteio = random.randint(0, 9999)
-
-aposta_ultimos_dois = aposta % 100
-sorteio_ultimos_dois = sorteio % 100
-
-
-animal_aposta = obter_animal(aposta_ultimos_dois)
-animal_sorteado = obter_animal(sorteio_ultimos_dois)
-
-print("\033[1;36mNúmero sorteado:\033[m", sorteio)
-print("\033[1;36mAnimal sorteado:\033[m", animal_sorteado)
-print("\033[33mSeu número apostado:\033[m", aposta)
-print("\033[33mSeu animal apostado:\033[m", animal_aposta)
-
-
-if aposta == sorteio:
-    print("\033[0;32mParabéns, você ganhou a aposta!\033[m")
-elif animal_aposta == animal_sorteado:
-    print('\033[0;32mVocê acertou o grupo do animal!\033[m, \033[1;31mmas errou o número.\033[m')##mod
-elif animal_aposta == animal_sorteado and aposta == sorteio:
-    print('\033[0;32mVocê acertou o número e o grupo do animal!\033[m')
-else:
-    print("\033[1;31mDesculpe, você perdeu a aposta.\033[m")
-
-
-
+if modalidade == 1: #dezena
+    numero = None
+    numero_gerado = random.randint(10,9999)
+    while numero not in range(10, 100):
+        numero = int(input("🔟Digite uma dezena jogar na modalidade Dezena: "))
+        aposta_ultimos_dois = numero % 100
+        sorteio_ultimos_dois = numero_gerado % 100
+        animal_aposta = obter_animal(aposta_ultimos_dois)
+        animal_sorteado = obter_animal(sorteio_ultimos_dois)
+    print(f'\033[36mO número sorteado foi:\033[0m "\033[32m{numero_gerado}\033[0m"')
+    print(f'\033[36mDezena do número sorteado é:\033[0m "\033[32m{numero_gerado%100}\033[0m"') #aqui ele pega os dois últimos dígitos
+    print("\033[1;36mAnimal sorteado:\033[m", animal_sorteado)
+    print("-"* 100)
+    print(f'\033[35mDezena apostada:\033[m{numero}')
+    print(f'\033[35mAnimal correspondente do número apostado:\033[m{obter_animal(numero)}')
 
